343224609106f2503fc219aa4eb834698b97b257
YuLim2 <jyulim0120@gmail.com>
2021-12-27 22:58:38 +0900

    add: 절차대로 추가

--- Boostcourse_Java/OkJavaGoInHome.java
+++ Boostcourse_Java/OkJavaGoInHome.java
@@ -1,9 +1,29 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
 
 public class OkJavaGoInHome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String id = "JAVA APT 507";
+		
+		// Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForup(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		
+		// Light on
+		Lighting hallLamp = new Lighting(id+" Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" Floor Lamp");
+		floorLamp.on();
 	}
 
 }
+ 
