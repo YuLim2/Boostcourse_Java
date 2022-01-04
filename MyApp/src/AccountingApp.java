968142c85760caf2340da9692b1e24ce9d11e12e
YuLim2 <jyulim0120@gmail.com>
2022-01-04 21:52:50 +0900

    add: 제어문을 추가하여 이익에 따라 수익 배분을 다르게 설정

--- MyApp/src/AccountingApp.java
+++ MyApp/src/AccountingApp.java
@@ -2,16 +2,27 @@ public class AccountingApp {
  
     public static void main(String[] args) {
  
-        double valueOfSupply = 10000.0;
+        double valueOfSupply = Double.parseDouble(args[0]);
         double vatRate = 0.1;
         double expenseRate = 0.3;
         double vat = valueOfSupply * vatRate;
         double total = valueOfSupply + vat;
         double expense = valueOfSupply * expenseRate;
         double income = valueOfSupply - expense;
-        double dividend1 = income * 0.5;
-        double dividend2 = income * 0.3;
-        double dividend3 = income * 0.2;
+        double dividend1;
+        double dividend2;
+        double dividend3;
+        
+        if(income > 10000) {
+        	dividend1 = income * 0.5;
+        	dividend2 = income * 0.3;
+        	dividend3 = income * 0.2;
+        }
+        else {
+        	dividend1 = income * 1.0;
+        	dividend2 = income * 0;
+        	dividend3 = income * 0;
+        }
  
         System.out.println("Value of supply : " + valueOfSupply);
         System.out.println("VAT : " + vat);
